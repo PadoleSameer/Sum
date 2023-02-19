5d1712e247655ec40b8bd4f8c3e2a5d162a1dace
Padole <spadole@dxc.com>
2023-02-19 20:09:56 +0530

    sum project

---
+++ src/test/java/Summation/SumOfTwoNos.java
@@ -0,0 +1,18 @@
+package Summation;
+
+public class SumOfTwoNos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int n1=10, n2=20;
+		
+		int sum = n1+n2;
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
